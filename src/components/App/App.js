99e241d541046543c96541c30584c6cf75815535
patrickrnelson <patrickrnelson1@gmail.com>
2021-03-05 18:30:50 -0600

    MovieDetails and NewMovieForm components added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,26 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
-import MovieList from '../MovieList/MovieList'
+import MovieList from '../MovieList/MovieList';
+import MovieDetails from '../MovieDetails/MovieDetails';
+import NewMovieForm from '../NewMovieForm/NewMovieForm'
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
       <Router>        
+        {/* Homepage */}
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
         {/* Details page */}
-
+        <Route path="/details" exact>
+          <MovieDetails />
+        </Route>
         {/* Add Movie page */}
+        <Route path="/addmovie" exact>
+          <NewMovieForm />
+        </Route>
       </Router>
     </div>
   );
