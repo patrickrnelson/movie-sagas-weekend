7c505d7fba697da48de214a7fae5f6ee94b5984d
patrickrnelson <patrickrnelson1@gmail.com>
2021-03-06 23:58:41 -0600

    working on button nav color change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory, Link, useParams } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 import HomeIcon from '@material-ui/icons/Home';
 import AddBoxIcon from '@material-ui/icons/AddBox';
 
@@ -13,28 +14,38 @@ function Header() {
   const [homeColor, setHomeColor] = useState("default");
   const [addColor, setAddColor] = useState("default");
 
-  const homeClick = () => {
-    setHomeColor("primary");
-    setAddColor("default");
-  }
-
-  const addClick = () => {
-    setHomeColor("default");
+  let { id } = useParams();
+  if(id === "addmovie") {
+    
     setAddColor("primary");
   }
+
+  console.log('id', id);
+
+  // const homeClick = () => {
+  //   setHomeColor("primary");
+  //   setAddColor("default");
+  // }
+
+  // const addClick = () => {
+  //   setHomeColor("default");
+  //   setAddColor("primary");
+  // }
 
   console.log('location', location);
   return (
     <div id="header-container">
       <h1 id="app-title">The Movies Saga</h1>
       
-      <Button component={ Link } to="/" color={homeColor} onClick={homeClick}>
-        <HomeIcon />Home
-      </Button>
+      <Box m={1}>
+        <Button component={ Link } to="/" color={homeColor} >
+          <HomeIcon />Home
+        </Button>
       
-      <Button component={ Link } to="/addmovie" color={addColor} onClick={addClick}>
-      <AddBoxIcon />Add Movie
-      </Button>
+        <Button component={ Link } to="/addmovie" color={addColor} >
+          <AddBoxIcon />Add Movie
+        </Button>
+      </Box>
     </div>
   )
 }
